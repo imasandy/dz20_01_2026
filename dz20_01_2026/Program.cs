11bc51800d9ccd608dc9bb3ad1e67f34c7fefdfc
Вениамин Литвин <veniaminlitvin993@gmail.com>
2026-01-31 23:43:32 +0200

    upd 2: added cols/rows swap

--- dz20_01_2026/Program.cs
+++ dz20_01_2026/Program.cs
@@ -14,23 +14,56 @@ public class dz20_01_2026
         while (true)
         {
             Console.Write("1. Вывод\n2. Заполнить рандомными числами" +
-                "\n3. Транспонирование\n4. Заполнить цифрами 1, 2, 3...\n> ");
-            string var = Console.ReadLine();
-            if (var == "1")
+                "\n3. Транспонирование\n4. Заполнить цифрами 1, 2, 3..." +
+                "\n5. Поменять столбцы\n6. Поменять строки\n> ");
+
+            int var;
+            while (!int.TryParse(Console.ReadLine(), out var))
+            {
+                Console.Write("Ошибка!: ");
+            }
+
+            if (var == 1)
             {
                 Render(matrix);
             }
-            else if (var == "2")
+            else if (var == 2)
             {
-                Random_Int(matrix, rnd);
+                RandomInt(matrix, rnd);
             }
-            else if (var == "3")
+            else if (var == 3)
             {
                 matrix = Transposition(matrix);
             }
-            else if (var == "4")
+            else if (var == 4)
             {
-                matrix = Regular_count(matrix);
+                matrix = RegularCount(matrix);
+            }
+            else if (var == 5)
+            {
+                Console.Write("Введите номер первого столбца: ");
+                int c1 = int.Parse(Console.ReadLine()) - 1;
+
+                Console.Write("Введите номер второго столбца: ");
+                int c2 = int.Parse(Console.ReadLine()) - 1;
+
+                if (c1 >= 0 && c1 < matrix.GetLength(1) && c2 >= 0 && c2 < matrix.GetLength(1))
+                    SwapColumns(matrix, c1, c2);
+                else
+                    Console.WriteLine("Неверный номер столбца!");
+            }
+            else if (var == 6)
+            {
+                Console.Write("Введите номер первой строки: ");
+                int r1 = int.Parse(Console.ReadLine()) - 1;
+
+                Console.Write("Введите номер второй строки: ");
+                int r2 = int.Parse(Console.ReadLine()) - 1;
+
+                if (r1 >= 0 && r1 < matrix.GetLength(0) && r2 >= 0 && r2 < matrix.GetLength(0))
+                    SwapRows(matrix, r1, r2);
+                else
+                    Console.WriteLine("Неверный номер строки!");
             }
         }
     }
@@ -42,7 +75,7 @@ public class dz20_01_2026
 
         while (!int.TryParse(Console.ReadLine(), out h) || h <= 0)
         {
-            Console.WriteLine("Ошибка");
+            Console.WriteLine("Ошибка: ");
         }
 
         Console.Write("Введите ширину: ");
@@ -53,7 +86,7 @@ public class dz20_01_2026
         }
     }
 
-    static void Random_Int(int[,] matrix, Random rnd)
+    static void RandomInt(int[,] matrix, Random rnd)
     {
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
@@ -115,7 +148,7 @@ public class dz20_01_2026
         return transposed;
     }
 
-    static int[,] Regular_count(int[,] matrix)
+    static int[,] RegularCount(int[,] matrix)
     {
         int count = 1;
         for (int i = 0; i < matrix.GetLength(0); i++)
@@ -127,6 +160,30 @@ public class dz20_01_2026
             }
         }
         return matrix;
+    }
+
+    static void SwapColumns(int[,] matrix, int col1, int col2)
+    {
+        int rows = matrix.GetLength(0);
+
+        for (int i = 0; i < rows; i++)
+        {
+            int temp = matrix[i, col1];
+            matrix[i, col1] = matrix[i, col2];
+            matrix[i, col2] = temp;
+        }
+    }
+
+    static void SwapRows(int[,] matrix, int row1, int row2)
+    {
+        int cols = matrix.GetLength(1);
+
+        for (int j = 0; j < cols; j++)
+        {
+            int temp = matrix[row1, j];
+            matrix[row1, j] = matrix[row2, j];
+            matrix[row2, j] = temp;
+        }
     }
 }
 
